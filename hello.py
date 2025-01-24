88e68ba6b44f690830d97cdd9d7c9326f18f2f36
Mathias Seipel <mseipel986@gmail.com>
2025-01-24 22:52:34 +0100

    Script change to more output

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
 print('Hello World')
+# Define a list
+fruits = ["Apple", "Banana", "Cherry", "Date", "Elderberry"]
+
+# Loop through the list and print each item
+for fruit in fruits:
+    print(fruit)
