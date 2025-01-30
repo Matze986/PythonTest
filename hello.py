650c1048ae2c15ab044f1a11227e26844d2eafc1
Mathias Seipel <mseipel986@gmail.com>
2025-01-30 20:45:06 +0100

    test

--- hello.py
+++ hello.py
@@ -2,40 +2,6 @@ import json
 import sys
 import subprocess
 
-# Sample JSON data
-json_string = '''
-{
-    "DoEncryptModules": false,
-    "Version": "1.0.27",
-    "FileType": 1,
-    "ContainerName": "1.0.27-alpha",
-    "Description": "1.0.27-alpha",
-    "Authors": ["Mathias Seipel"],
-    "Tags": ["Level:1", "SN:A", "#01:ABC"],
-    "DependencyGroups": [
-        {
-            "targetFramework": "any",
-            "dependencies": [
-                {"id": "AMP", "range": "[1.0.0, 1.0.0]"},
-                {"id": "ESP_GEN", "range": "[1.0.0, 1.0.0]"},
-                {"id": "DM_RL", "range": "[1.0.0, 1.0.0]"}
-            ]
-        }
-    ],
-    "StateID": 2,
-    "ReleaseNotes": "Published from AMP 1.0.27-alpha",
-    "CurrentAuthorId": 1,
-    "ID": 5,
-    "Name": "AMP",
-    "LatestVersion": "1.0.27",
-    "IsPersonal": false,
-    "PublishedState": 0
-}
-'''
-
-
-
-
 # Build form data object
 def flatten_json(obj, prefix=''):
     """Recursively flattens nested JSON into key-value pairs using dot notation and bracket indices."""
@@ -71,6 +37,13 @@ def main(PackageMetadata, PackageContentS3Key, Email, BaseUrl, parsed_data):
     base_urls = get_base_package_service_url(BaseUrl)
     package_service_base_url = base_urls[0]
     minio_base_url = base_urls[1]
+    
+    # Parse JSON
+    try:
+        parsed_data = json.loads(PackageMetadata)
+    except json.JSONDecodeError:
+        print("Invalid JSON input")
+        sys.exit(1)
 
     # Flatten JSON
     flattened_data = flatten_json(parsed_data)
@@ -84,14 +57,15 @@ def main(PackageMetadata, PackageContentS3Key, Email, BaseUrl, parsed_data):
     curl_command = curl_command.rstrip(" \\\n")
 
     print(curl_command)
+    result = subprocess.run(curl_command, capture_output=True, text=True)
+
 
 if __name__ == "__main__":
     PackageMetadata = sys.argv[1]
     PackageContentS3Key = sys.argv[2]
     Email = sys.argv[3]
     BaseUrl = sys.argv[4]
-    # Parse JSON
-    PackageMetadataAsJsonString = json.dumps(PackageMetadata)
-    parsed_data = json.loads(PackageMetadataAsJsonString)
-    main(PackageMetadata, PackageContentS3Key, Email, BaseUrl, parsed_data)
+    
+    #json_string = json.dumps(parsed_data)
+    main(PackageMetadata, PackageContentS3Key, Email, BaseUrl)
 
